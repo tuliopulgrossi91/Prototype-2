486a893148df252778ef60695abb86a8893ee835
Tulio Pulgrossi <tuliopulgrossi91@gmail.com>
2019-10-31 10:42:33 -0300

    Update
    Prototype 2 - Quiz Game

---
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject Questions, Pause, Settings, Results;
+    private readonly bool finish;
+
+    public void QuestionsGame(bool q) => Questions.SetActive(q); // questions game
+    public void OnApplicationPause(bool pause) => Pause.SetActive(pause); // pause game
+    public void SettingsGame(bool s) => Settings.SetActive(s); // settings game
+    public void RetryGame() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // retry game
+    public void BackLevels() => SceneManager.LoadScene(1); // back levels game
+
+    void Update()
+    {
+        if (finish == true) // game finish - show results
+        {
+            Questions.SetActive(false);
+            Results.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Levels.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levels : MonoBehaviour
+{
+    public GameObject LevelManager, Settings;
+
+    public void LevelGame(bool l) => LevelManager.SetActive(l); // level game
+    public void SettingsGame(bool s) => Settings.SetActive(s); // settings game
+    public void LoadLevel() => SceneManager.LoadScene(2); // load level game
+    public void BackMenu() => SceneManager.LoadScene(0); // back menu game
+}
